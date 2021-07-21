01272c4ef60c842f538a099c67909669ad4ce316
emipa606 <emil.offermo@litet.net>
2021-07-21 12:42:26 +0200

    Updated for 1.3, replaced JecsTools dependency.

---
+++ Source/Cleave/DamageDefCleave.cs
@@ -0,0 +1,12 @@
+﻿using Verse;
+
+namespace MeleePlease
+{
+    public class DamageDefCleave : DamageDef
+    {
+        public float armorPenetration = 0f;
+        public DamageDef cleaveDamage = null;
+        public float cleaveFactor = 0.7f; //Damage factor for the cleave attack
+        public int cleaveTargets = 0; //Number of bonus targets
+    }
+}
